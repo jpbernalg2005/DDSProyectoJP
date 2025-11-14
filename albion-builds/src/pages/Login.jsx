2b160c23d06c01d0d4c4c043456b8b48c1c1bacc
jpbernalg2005 <jpbernalg2005@gmail.com>
2025-11-13 22:16:57 -0300

    Cambios logo y logo de la pestaña

--- albion-builds/src/pages/Login.jsx
+++ albion-builds/src/pages/Login.jsx
@@ -31,7 +31,6 @@ export default function Login({ setIsLoggedIn, users }) {
                 <img 
                     src={LogoImage} 
                     alt="Albion Builder Logo" 
-                    style={{ height: '50px' }} 
                 />
             </div>
         
